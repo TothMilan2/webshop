c487f0dfb0f7f76495ebf532e1c29ffd395ee59e
TothMilan <tothmilan@turr.hu>
2025-03-06 09:15:46 +0100

    biggestoldaleverexisted

--- kosar.js
+++ kosar.js
@@ -17,3 +17,17 @@ hambi.addEventListener("click", function(event) {
         hambi.classList.remove('active')
         
     }});
+
+
+    window.addEventListener('DOMContentLoaded', function() {
+        // Retrieve the image URL from sessionStorage
+        var imageUrl = sessionStorage.getItem('megrendelkep');
+        
+        // If there's an image URL stored in sessionStorage, set it to the img tag
+        if (imageUrl) {
+            document.getElementById('kosarkepecske').src = imageUrl;
+        } else {
+            // If there's no image URL in sessionStorage, alert or handle the case
+            alert('No image found in the cart.');
+        }
+    });
